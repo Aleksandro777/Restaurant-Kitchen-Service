1a285e547a1486280a227652e335464f70b90f88
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-11-10 09:39:22 +0200

    Display fields dish_type & price in DishAdmin

--- kitchen/models.py
+++ kitchen/models.py
@@ -13,11 +13,11 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True, blank=True)
 
     class Meta:
-        verbose_name = "cook"
-        verbose_name_plural = "cooks"
+        verbose_name = "Cook"
+        verbose_name_plural = "Cooks"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
@@ -26,7 +26,7 @@ class Cook(AbstractUser):
 class Dish(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(
         DishType,
         on_delete=models.CASCADE
@@ -35,6 +35,8 @@ class Dish(models.Model):
 
     class Meta:
         ordering = ["name"]
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
 
     def __str__(self) -> str:
         return self.name
