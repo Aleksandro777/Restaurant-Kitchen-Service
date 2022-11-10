7014a72aecfeb5a8e6ebffed2844f49f40545807
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-11-10 11:47:27 +0200

    Add home page with content

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes
+    }
+
+    return render(request, "kitchen/index.html", context=context)
+
