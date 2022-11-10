fa7187da317ce39753a43fc309595984fb98983a
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-11-10 13:08:54 +0200

    Add cook detail page

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    CookListView,
+    DishDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dishtype/",
+         DishTypeListView.as_view(),
+         name="dish-type-list"),
+    path("dish/",
+         DishListView.as_view(),
+         name="dish-list"),
+    path("cook/",
+         CookListView.as_view(),
+         name="cook-list"),
+    path("dish/<int:pk>/",
+         DishDetailView.as_view(),
+         name="dish-detail"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Cook, Dish, DishType
 
@@ -11,8 +12,29 @@ def index(request):
     context = {
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
-        "num_dishes": num_dishes
+        "num_dishes": num_dishes,
     }
 
     return render(request, "kitchen/index.html", context=context)
 
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dishes_type_list"
+    template_name = "kitchen/dishes_type_list.html"
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 5
+    queryset = Dish.objects.select_related("dish_type")
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
