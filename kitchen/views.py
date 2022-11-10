baf89c91e9ca8abfe5d641c552ea9ad79fdd8d0b
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-11-10 14:02:55 +0200

    Add login and logged out in sidebar

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
 from kitchen.models import Cook, Dish, DishType
 
 
+@login_required
 def index(request):
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
@@ -18,29 +21,29 @@ def index(request):
     return render(request, "kitchen/index.html", context=context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dishes_type_list"
     template_name = "kitchen/dishes_type_list.html"
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     paginate_by = 5
     queryset = Dish.objects.select_related("dish_type")
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
-    paginate_by = 5
+    paginate_by = 1
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
 
 
-class CookDetailView (generic.DetailView):
+class CookDetailView (LoginRequiredMixin, generic.DetailView):
     model = Cook
     context_object_name = "cooks"
     queryset = Cook.objects.prefetch_related("dishes__dish_type")
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("kitchen.urls", namespace="kitchen"))
+    path("", include("kitchen.urls", namespace="kitchen")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
